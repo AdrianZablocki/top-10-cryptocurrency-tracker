7c51422f8a3e58247d82f7130b769582f50d4826
Adrian Zablocki <adrian@MacBook-Pro-Adrian.local>
2018-07-15 12:06:50 +0200

    Setup environment with create-react-app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Top 10 Cryptocurrency Tracker App (react)
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+To start the app:
+1. Clone the repository
+2. `npm install`
+3. `npm start` 
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Top 10 cryptocurrency tracker!!!
+      </div>
+    );
+  }
+}
+
+export default App;
